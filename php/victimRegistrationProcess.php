0c83dd24eba144003256519e943db230816915c5
Evans Kiptarus Kibet <evanskibet114@gmail.com>
2018-01-06 09:55:02 +0300

    Update victimRegistrationProcess.php

--- php/victimRegistrationProcess.php
+++ php/victimRegistrationProcess.php
@@ -1,7 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Acme web design | Services</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="css/loginNot-style.css" type="text/css">
+    <meta name="viewport" content="width=device-wisth">
+
+    <meta name="description" content="responsive web device">
+    <meta name="keywords" content="responsive web , affordable website, good website">
+    <meta name="author" content="Evans Kiptarus kibet">
+</head>
+<body>
 <?php
 //get the victims information
 include '../db-header.php';
-
+if(isset($_POST['submit'])){
+$target="../images/".basename($_FILES["image"]["name"]);
 $fname=$_POST['fullname'];
 $victimid=$_POST['victimID'];
 $date=$_POST['dob'];
@@ -10,6 +25,8 @@ $lastseen=$_POST['lastSeen'];
 $h=$_POST['height'];
 $cloth=$_POST['clothing'];
 $description=$_POST['desc'];
+$img=$_FILES["image"]["name"];
+
 
 //get information for policeID
 $police=$_POST['policeID'];
@@ -31,17 +48,28 @@ else{
 	 echo "Wrong witness information";
 }else
 {
-	$sql= "insert into victim(fullname,victimid,dob,gender,lastseen,height,clothing,description) 
-	values('$fname','$victimid','$date','$gender','$lastseen','$h','$cloth','$description')";
+	$sql= "insert into victim(fullname,victimid,dob,gender,lastseen,height,clothing,description,img) 
+	values('$fname','$victimid','$date','$gender','$lastseen','$h','$cloth','$description','$img')";
 	$result2=$conn->query($sql);
-	if(!$result2){
-		echo "No information inserted to db";
-	}else{
+	if(move_uploaded_file($_FILES['image']['tmp_name'],$target)){
+		if($result2){
+			echo "<h2>Registration succeeed!</h2>";
+			header("Location: ../service.php");
+			
+		}else{
+			echo "No information inserted to db";
+		
+	}
 
-		echo "<h2>Registration succeeed!</h2>";
-		header("Location: ../service.php");
+	}else{
+		echo "Image failed to be loaded".mysql_error();
+	}
 }
 }
+}else{
+	echo "No dir selected".mysql_error();
 }
 
 ?>
+</body>
+</html>
